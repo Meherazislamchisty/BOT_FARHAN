828a7d9a1174f7fb80ad40baa3ec43de4dcb8e95
MEHERAZ ISLAM <meherazchisty57@gmail.com>
2025-10-14 14:26:46 +0600

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -116,7 +116,7 @@ module.exports = {
           "╔══『 𝐏𝐑𝐄𝐅𝐈𝐗 』══╗\n"
         + `║ 🌍 System : ${globalPrefix}\n`
         + `║ 💬 Chatbox : ${threadPrefix}\n`
-        + `║ ➤ ${threadPrefix}help to see all available cmds 🥵\n`
+        + `║ ➤ ${threadPrefix}help to see all available cmds 🧠\n`
         + "╚═══════════════╝",
         attachment: await utils.getStreamFromURL("https://drive.google.com/uc?export=download&id=1xljsQQl2cp1PKW5jyhsnWabxuQ49RMXl")
       });
