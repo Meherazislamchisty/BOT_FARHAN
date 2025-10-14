409fd2764ca622998076a808443fec23c25dbfcd
MEHERAZ ISLAM <meherazchisty57@gmail.com>
2025-10-14 20:34:26 +0600

    Update and rename ayan.js to meheraz.js

--- scripts/cmds/meheraz.js
+++ scripts/cmds/meheraz.js
@@ -12,7 +12,7 @@ category: "no prefix",
 
 onStart: async function(){}, 
 onChat: async function({ event, message, getLang }) {
-if (event.body && event.body.toLowerCase() === "meheraz" "chisty") {
+if (event.body && event.body.toLowerCase() === "meheraz" ) {
 return message.reply({
 body: " ╭────────────◊\n├‣ 𝐎𝐰𝐧𝐞𝐫 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧  \n├───────────◊ \n├‣ 𝐍𝐚𝐦𝐞 : 𝑴𝒆𝒉𝒆𝒓𝒂𝒛 𝑰𝒔𝒍𝒂𝒎 𝑪𝒉𝒊𝒔𝒕𝒚  \n├‣ 𝐇𝐨𝐦𝐞 : ᴄʜᴀᴛᴛᴏɢʀᴀᴍ \n├‣ 𝐂𝐥𝐚𝐬𝐬 : Ssᴄ ᴅᴏɴᴇ \n╰───────────◊  ",
 attachment: await global.utils.getStreamFromURL("https://files.catbox.moe/87d2eo.mp4")
