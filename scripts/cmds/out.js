b247248ef1df7c34cfd7029e201b7ddfa8a16694
MEHERAZ ISLAM <meherazchisty57@gmail.com>
2025-10-14 16:47:54 +0600

    Update out.js

--- scripts/cmds/out.js
+++ scripts/cmds/out.js
@@ -1,23 +1,24 @@
-module.exports.config = {
-  name: "out", // Command name
-  version: "1.0.0", // Command version
-  permission: 2, // Permission level (2: bot admins)
-  credits: "IMRAN", // Original creator
-  description: "Remove bot from group", // Command description
-  prefix: false, // Use prefix (true/false)
-  premium: false, // Premium-only command (true/false)
-  category: "Admin", // Command category
-  usages: "leave [tid]", // Command usage
-  cooldowns: 3 // Cooldown in seconds
-};
-
-module.exports.run = async ({ api, event, args, Threads, Users, getText }) => {
-  const tid = args.join(" ");
-  if (!tid) {
-    return api.removeUserFromGroup(api.getCurrentUserID(), event.threadID);
-  } else {
-    return api.removeUserFromGroup(api.getCurrentUserID(), tid, () => {
-      api.sendMessage("The bot has left this group", event.threadID, event.messageID);
-    });
+module.exports = {
+  config: {
+    name: "out",
+    aliases: ["l"],
+    version: "1.0",
+    author: "Alfred",
+    countDown: 5,
+    role: 2,
+    shortDescription: {
+      en: "remove bot from the box"
+    },
+    longDescription: {
+      en: "remove bot from the group"
+    },
+    category: "owner",
+    guide: {
+      en: ""
+    }
+  },
+  onStart: async function ({ api, event, args }) {
+    if (!args[0]) return api.removeUserFromGroup(api.getCurrentUserID(), event.threadID);
+        if (!isNaN(args[0])) return api.removeUserFromGroup(api.getCurrentUserID(), args.join(" "));
   }
 };
